5986ada9d913e46cf4dfe74af07de117400fe815
Ben Abrams <me@benabrams.it>
2019-03-11 20:07:45 -0700

    some cleanup/housekeeping
    I noticed that the travis deploy setup was not properly picking up the cookbook name so I set it explicitly.
    
    Added and updated some other misc stuff in the metadata to match reality (for example the LICENSE in repo was MIT but the attribute in metadata said all rights reserved), chef version support, issue url, source code url, etc.
    
    Signed-off-by: Ben Abrams <me@benabrams.it>

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,15 @@
 name             'netuitive'
 maintainer       'Ben Abrams'
 maintainer_email 'me@benabrams.it'
-license          'All rights reserved'
+license          'MIT'
 description      'Installs/Configures netuitive'
 long_description 'Installs/Configures netuitive'
+
+# TODO: drop chef 12 support, chef 13 is about to be EOL
+chef_version     '>= 12.5' if respond_to?(:chef_version)
+issues_url       'https://github.com/Netuitive/chef-netuitive/issues' if respond_to?(:issues_url)
+source_url       'https://github.com/Netuitive/chef-netuitive' if respond_to?(:source_url)
+
 version          '0.21.0'
 
 depends 'apt'
